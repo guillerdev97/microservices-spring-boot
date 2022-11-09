eba586591d9fb55ae26d946e2eb7ea57ae29ffd7
Guillermo García <guillermieres97@gmail.com>
2022-11-09 10:21:28 +0100

    create saveDepartment method of the DepartmentController and DepartmentService

--- src/main/java/com/guillerdev97/department/controller/DepartmentController.java
+++ src/main/java/com/guillerdev97/department/controller/DepartmentController.java
@@ -1,17 +1,30 @@
 package com.guillerdev97.department.controller;
 
+import com.guillerdev97.department.entity.Department;
 import com.guillerdev97.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/v1/departments")
+@Slf4j
 public class DepartmentController {
     private final DepartmentService departmentService;
 
     @Autowired
-    public DepartmentController(DepartmentService departmentService) {
+    public DepartmentController(Logger log, DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of the DepartmentController");
+        return departmentService.saveDepartment(department);
     }
 }
--- src/main/java/com/guillerdev97/department/service/DepartmentService.java
+++ src/main/java/com/guillerdev97/department/service/DepartmentService.java
@@ -1,15 +1,23 @@
 package com.guillerdev97.department.service;
 
+import com.guillerdev97.department.entity.Department;
 import com.guillerdev97.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
     private final DepartmentRepository departmentRepository;
 
     @Autowired
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment method of the DepartmentService");
+        return departmentRepository.save(department);
     }
 }
